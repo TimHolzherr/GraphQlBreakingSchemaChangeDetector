98f0589983899d6dfe81a45cf15fe080737d6dae
Tim Holzherr <tim.holzherr@swisslife.ch>
2022-03-18 14:55:41 +0100

    add new tests

---
+++ test/SchemaCompare.Tests/ChangingEnumValues.cs
@@ -0,0 +1,174 @@
+﻿using Xunit;
+
+namespace SchemaCompare.Tests;
+
+public class ChangingEnumValues
+{
+    private readonly SchemaComparer _schemaComparer = new();
+
+    [Fact]
+    public void RemoveEnumValueInInputType_CheckRules_BreakingChange()
+    {
+        // Arrange
+        var oldSchema = @"
+                            type mutation {
+                                foo(bar: Baz!): FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: String!
+                            }
+
+                            enum Baz {
+                                FirstValue
+                                SecondValue
+                                ThirdValue
+                            }
+                        ";
+        var newSchema = @"
+                            type mutation {
+                                foo(bar: Baz!): FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: String!
+                            }
+
+                            enum Baz {
+                                FirstValue                                
+                                ThirdValue
+                            }
+                        ";
+        
+        // Act
+        var breakingChanges = _schemaComparer.DetectBreakingChanges(oldSchema, newSchema);
+
+        // Assert
+        Assert.Equal(1, breakingChanges.Count);
+    }
+
+    [Fact]
+    public void AddEnumValueInInputType_CheckRules_NoBreakingChange()
+    {
+        // Arrange
+        var oldSchema = @"
+                            type mutation {
+                                foo(bar: Baz!): FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: String!
+                            }
+
+                            enum Baz {
+                                FirstValue
+                                SecondValue                                
+                            }
+                        ";
+        var newSchema = @"
+                            type mutation {
+                                foo(bar: Baz!): FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: String!
+                            }
+
+                            enum Baz {
+                                FirstValue 
+                                SecondValue
+                                ThirdValue
+                            }
+                        ";
+
+        // Act
+        var breakingChanges = _schemaComparer.DetectBreakingChanges(oldSchema, newSchema);
+
+        // Assert
+        Assert.Equal(0, breakingChanges.Count);
+    }
+
+    [Fact]
+    public void AddEnumValueInOutputType_CheckRules_BreakingChange()
+    {
+        // Arrange
+        var oldSchema = @"
+                            type query {
+                                foo: FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: Baz!
+                            }
+
+                            enum Baz {
+                                FirstValue
+                                SecondValue                                
+                            }
+                        ";
+
+        var newSchema = @"
+                            type query {
+                                foo: FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: Baz!
+                            }
+
+                            enum Baz {
+                                FirstValue
+                                SecondValue
+                                ThirdValue
+                            }
+                        ";
+
+        // Act
+        var breakingChanges = _schemaComparer.DetectBreakingChanges(oldSchema, newSchema);
+
+        // Assert
+        Assert.Equal(1, breakingChanges.Count);
+    }
+
+    [Fact]
+    public void RemoveEnumValueInOutputType_CheckRules_NoBreakingChange()
+    {
+        // Arrange
+        var oldSchema = @"
+                            type query {
+                                foo: FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: Baz!
+                            }
+
+                            enum Baz {
+                                FirstValue
+                                SecondValue  
+                                ThirdValue
+                            }
+                        ";
+
+        var newSchema = @"
+                            type query {
+                                foo: FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: Baz!
+                            }
+
+                            enum Baz {
+                                FirstValue
+                                SecondValue                                
+                            }
+                        ";
+
+        // Act
+        var breakingChanges = _schemaComparer.DetectBreakingChanges(oldSchema, newSchema);
+
+        // Assert
+        Assert.Equal(0, breakingChanges.Count);
+    }
+}
---
+++ test/SchemaCompare.Tests/DisallowAddingNewMandatoryInputFields.cs
@@ -0,0 +1,88 @@
+﻿using Xunit;
+
+namespace SchemaCompare.Tests;
+
+public class DisallowAddingNewMandatoryInputFields
+{
+    private readonly SchemaComparer _schemaComparer = new();
+
+    [Fact]
+    public void AddNewMandatoryInputField_CheckRules_BreakingChange()
+    {
+        // Arrange
+        var oldSchema = @"
+                            type mutation {
+                                foo(bar: Baz!): FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: String!
+                            }
+
+                            input Baz {
+                                value: String!
+                            }
+                        ";
+
+        var newSchema = @"
+                            type mutation {
+                                foo(bar: Baz!): FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: String!
+                            }
+
+                            input Baz {
+                                value: String!
+                                secondValue: String!
+                            }
+                        ";
+        
+        // Act
+        var breakingChanges = _schemaComparer.DetectBreakingChanges(oldSchema, newSchema);
+
+        // Assert
+        Assert.Equal(1, breakingChanges.Count);
+    }
+
+    [Fact]
+    public void AddNewOptionalInputField_CheckRules_NoBreakingChange()
+    {
+        // Arrange
+        var oldSchema = @"
+                            type mutation {
+                                foo(bar: Baz!): FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: String!
+                            }
+
+                            input Baz {
+                                value: String!
+                            }
+                        ";
+
+        var newSchema = @"
+                            type mutation {
+                                foo(bar: Baz!): FooPayload                                 
+                            }
+
+                            type FooPayload {
+                                value: String!
+                            }
+
+                            input Baz {
+                                value: String!
+                                secondValue: String
+                            }
+                        ";
+
+        // Act
+        var breakingChanges = _schemaComparer.DetectBreakingChanges(oldSchema, newSchema);
+
+        // Assert
+        Assert.Equal(0, breakingChanges.Count);
+    }
+}
